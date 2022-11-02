08bbbecf08a93510c1655b2d5f77afa52443a9fc
SosninRoman <sosninroman92@gmail.com>
2022-11-02 23:28:21 +0500

    cleanup

--- plugin/treelistview/src/plugin.h
+++ plugin/treelistview/src/plugin.h
@@ -6,20 +6,10 @@
 namespace treeview
 {
 
-/**
- * @brief The MyPlugin class. Simple qml plugin example.
- */
-
 class TreeViewPlugin: public QQmlExtensionPlugin {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID "tree/1.0")
 public:
-
-    /**
-     * @brief registerTypes Overrided function that should register all
-     * C++ classes exported by this plugin.
-     * @param uri           Plugin uri.
-     */
     void registerTypes(const char* uri) override;
 };
 
